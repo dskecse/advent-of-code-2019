2a8a5974e07e45c087b014bfeb69694cbda24712
Dzianis Dashkevich <dskecse@gmail.com>
2019-12-02 01:44:51 +0300

    Add solution for Day 1, Part 2

--- src/aoc2019s/core.clj
+++ src/aoc2019s/core.clj
@@ -1,42 +1,36 @@
 (ns aoc2019s.core
   (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-;; fuel required to launch a module is based on its mass
+;; fuel required for a given mass
 ;; mass / 3
 ;; round down
 ;; subtract 2
-(defn fuel-req [mass]
+(defn fuel [mass]
   ;; (println mass)
   ;; (println (type mass))
   ;; (println (read-string mass))
   (- (int (/ mass 3)) 2))
 
-;; problem
-;; find out total fuel requirement
-;; individually calculate the fuel needed for the mass of each module (puzzle input)
-;; then add them together
 ;; (defn module-masses
 ;;   (with-open [rdr (io/reader "input1.txt")]
 ;;     (doall (map fuel-req (line-seq rdr)))))
-(defn module-masses []
+(def module-masses
   (with-open [rdr (io/reader "input1.txt")]
     ;; (println (type rdr))
     ;; (println (type (line-seq rdr)))
     ;; (println (line-seq rdr))
     ;; (Integer/parseInt s) (read-string s)
-    (doall (map fuel-req (map read-string (line-seq rdr))))))
+    (doall (map read-string (line-seq rdr)))))
 
-(defn total-fuel-req []
-  (reduce + 0 (module-masses)))
+;; problem
+;; find out total fuel requirement
+;; individually calculate the fuel needed for the mass of each module (puzzle input)
+;; then add them together
+;; (def module-fuel
+;;   (map fuel module-masses))
 
-;; #error {
-;;  :cause Call to clojure.core/defn did not conform to spec.
-;;
-;; https://stackoverflow.com/questions/58291671/call-to-clojure-core-defn-did-not-conform-to-spec
-(defn masses []
-  0)
+(defn total-module-fuel [module-mass]
+  (reduce + 0 (take-while pos? (iterate fuel (fuel module-mass)))))
+
+(def total
+  (reduce + 0 (map total-module-fuel module-masses)))
--- test/aoc2019s/core_test.clj
+++ test/aoc2019s/core_test.clj
@@ -2,8 +2,13 @@
   (:require [clojure.test :refer :all]
             [aoc2019s.core :refer :all]))
 
-(deftest fuel-required
-  (is (= 2 (fuel-req 12)))
-  (is (= 2 (fuel-req 14)))
-  (is (= 654 (fuel-req 1969)))
-  (is (= 33583 (fuel-req 100756))))
+(deftest fuel-required-for-mass
+  (is (= 2 (fuel 12)))
+  (is (= 2 (fuel 14)))
+  (is (= 654 (fuel 1969)))
+  (is (= 33583 (fuel 100756))))
+
+(deftest total-fuel-required-for-module-mass
+  (is (= 2 (total-module-fuel 12)))
+  (is (= 966 (total-module-fuel 1969)))
+  (is (= 50346 (total-module-fuel 100756))))
