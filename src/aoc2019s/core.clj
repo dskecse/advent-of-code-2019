8eb273f26a401fa7c4f11e7a91ffefabf5e01fe3
Dzianis Dashkevich <dskecse@gmail.com>
2019-12-02 22:52:26 +0300

    Move Day 1 solution to aoc2019s.day1

--- src/aoc2019s/core.clj
+++ src/aoc2019s/core.clj
@@ -1,36 +1,4 @@
 (ns aoc2019s.core
-  (:require [clojure.java.io :as io]))
+  (:require [aoc2019s.day1 :refer :all]))
 
-;; fuel required for a given mass
-;; mass / 3
-;; round down
-;; subtract 2
-(defn fuel [mass]
-  ;; (println mass)
-  ;; (println (type mass))
-  ;; (println (read-string mass))
-  (- (int (/ mass 3)) 2))
-
-;; (defn module-masses
-;;   (with-open [rdr (io/reader "input1.txt")]
-;;     (doall (map fuel-req (line-seq rdr)))))
-(def module-masses
-  (with-open [rdr (io/reader "input1.txt")]
-    ;; (println (type rdr))
-    ;; (println (type (line-seq rdr)))
-    ;; (println (line-seq rdr))
-    ;; (Integer/parseInt s) (read-string s)
-    (doall (map read-string (line-seq rdr)))))
-
-;; problem
-;; find out total fuel requirement
-;; individually calculate the fuel needed for the mass of each module (puzzle input)
-;; then add them together
-;; (def module-fuel
-;;   (map fuel module-masses))
-
-(defn total-module-fuel [module-mass]
-  (reduce + 0 (take-while pos? (iterate fuel (fuel module-mass)))))
-
-(def total
-  (reduce + 0 (map total-module-fuel module-masses)))
+(println total)
--- test/aoc2019s/core_test.clj
+++ test/aoc2019s/core_test.clj
@@ -1,14 +1,3 @@
 (ns aoc2019s.core-test
   (:require [clojure.test :refer :all]
             [aoc2019s.core :refer :all]))
-
-(deftest fuel-required-for-mass
-  (is (= 2 (fuel 12)))
-  (is (= 2 (fuel 14)))
-  (is (= 654 (fuel 1969)))
-  (is (= 33583 (fuel 100756))))
-
-(deftest total-fuel-required-for-module-mass
-  (is (= 2 (total-module-fuel 12)))
-  (is (= 966 (total-module-fuel 1969)))
-  (is (= 50346 (total-module-fuel 100756))))
